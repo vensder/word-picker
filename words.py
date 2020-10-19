f5b466a550e6af7062321cff70e80845f1ee5199
vensder <vensder@gmail.com>
2020-10-19 23:39:32 +1300

    added exclusions

--- words.py
+++ words.py
@@ -10,6 +10,11 @@ lbFont = ("Helvetica", 16)
 lbHeight = 30
 picked = ""
 
+exclusions_set = set()
+with open("exclusions.exc", "r") as f:
+    exclusions = f.read()
+    exclusions_set = set(exclusions.split())
+
 
 def clear_text(text, chars):
     char_list = list(chars)
@@ -20,8 +25,7 @@ def clear_text(text, chars):
     return cleaned_text
 
 
-# This is where we lauch the file manager bar.
-def open_file():
+def open_subtitles():
     name = filedialog.askopenfilename(
         initialdir="./subtitles",
         filetypes=(("subtitles files", "*.srt"), ("all files", "*.*")),
@@ -33,21 +37,36 @@ def open_file():
             text, r'.-;:"&,0123456789%<>/\\!?'
         )  # TODO: replase with regex: remove not words etc.
         words = text.split()
-        my_set = set(words)
+        my_set = set(words) - exclusions_set
+        lb1.delete(0, END)
         for word in sorted(my_set):
             if len(word) > 2:
                 lb1.insert(END, word)
 
 
-def save_file():
+def save_picked_words():
     name = filedialog.asksaveasfilename(
         initialdir=".",
         title="Save file",
         filetypes=(("text files", "*.txt"), ("all files", "*.*")),
     )
     with open(name, "w") as f:
-        for item in get_words():
+        for item in get_picked_words():
             f.write(f"{item}\n")
+
+
+def save_exclusions():
+    new_exclusions_set = set(get_new_exclusions())
+    all_exclusions = exclusions_set | new_exclusions_set
+    with open("exclusions.exc", "w") as f:
+        for item in all_exclusions:
+            f.write(f"{item}\n")
+
+
+def show_exclusions():
+    lb1.delete(0, END)
+    for word in sorted(exclusions_set):
+        lb1.insert(END, word)
 
 
 def lb1_click(event):
@@ -66,8 +85,12 @@ def lb2_click(event):
     lb1.insert(END, picked)
 
 
-def get_words():
+def get_picked_words():
     return lb2.get(0, END)
+
+
+def get_new_exclusions():
+    return lb1.get(0, END)
 
 
 Title = root.title("Subtitles word picker")
@@ -76,8 +99,10 @@ Title = root.title("Subtitles word picker")
 menu = Menu(root)
 root.config(menu=menu)
 file = Menu(menu)
-file.add_command(label="Open subtitles", command=open_file)
-file.add_command(label="Save picked words", command=save_file)
+file.add_command(label="Open subtitles", command=open_subtitles)
+file.add_command(label="Save picked words", command=save_picked_words)
+file.add_command(label="Show exclusions", command=show_exclusions)
+file.add_command(label="Save exclusions", command=save_exclusions)
 file.add_command(label="Exit", command=lambda: exit())
 menu.add_cascade(label="File", menu=file)
 
